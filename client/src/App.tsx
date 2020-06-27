a0a855144f1ea80198349f056533f16bd5b70b0e
Juno <Laevino@gmail.com>
2020-06-27 13:49:35 -0400

    chore: reverted client changes to be those of master.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const handleClick = function() {
-
-  }
   return (
     <div className="App">
-
-
-<button>
-
-<img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png" alt=""/>
-            <a 
-              className="Login Button"
-              href='http://localhost:3001/auth/steam'
-            >
-              Login
-            </a>
-</button>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
