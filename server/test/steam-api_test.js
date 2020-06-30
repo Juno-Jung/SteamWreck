dbd397f9db4de7c567f1b8a76f7faafd8e38db7a
Juno <Laevino@gmail.com>
2020-06-30 10:37:52 -0400

    test: created new file for testing steam-api functions.

---
+++ server/test/steam-api_test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const steamApi = require('./../services/steam-api');
+
+const expect = require('chai').expect;
+
+describe('getUserSummary', () => {
+
+});
+
+describe('getUserLibrary', () => {
+
+});
+
+describe('getRecommendations', () => {
+
+});
