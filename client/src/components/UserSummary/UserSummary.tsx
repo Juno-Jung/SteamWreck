3273afc806e49613d8504732e09e0081b5de9942
Pat Rivet <patrickmrivet@gmail.com>
2020-06-30 12:36:26 +0100

    client: Added node-Sass module (package.json) and user summary shows name and avatar

--- client/src/components/UserSummary/UserSummary.tsx
+++ client/src/components/UserSummary/UserSummary.tsx
@@ -1,11 +1,29 @@
 import React, { FunctionComponent  } from 'react';
+import './UserSummary.scss';
 
 type UserSummaryProps = {
   //
 };
 
+interface userObj {
+  favourites: [];
+  personaname: string;
+  avatarfull: string;
+  countrycode: string;
+}
+
+const user: userObj = JSON.parse('{ "personaname": "Test", "avatarfull": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/ec/ecd7f7410cb1c7f59b62bf2355df6269ab8b14e1_full.jpg" }');
+
 const UserSummary: FunctionComponent = () => {
-  return <div>UserSummary</div>;
+  return (
+    <>
+      <div className="container">
+        <img className="avatarFull" src={user.avatarfull}/>
+        <div className="personaName">{user.personaname}</div>
+      </div>
+    </>
+  )
+
 };
 
 export default UserSummary;
