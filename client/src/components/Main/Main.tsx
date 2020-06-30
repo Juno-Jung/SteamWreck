e73341bbfbff32cbf397f53ca2894a8b77a5862b
Pat Rivet <patrickmrivet@gmail.com>
2020-06-30 16:44:37 +0100

    client: routing: Added Lena's changes of navbar- moved from App to Main.

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -4,15 +4,25 @@ import Navbar from '../Navbar/Navbar';
 import UserSummary from '../UserSummary/UserSummary';
 import RecommendationList from '../RecommendationList/RecommendationList';
 
-
 type MainProps = {
   //
 };
 
+const navigation = {
+  company: { name: "SteamWreck", to: "/" },
+  links: [
+    { name: "About", to: "/about" },
+    { name: "FaveWreck", to: "/favourite" },
+    { name: "WorstWreck", to: "/worst" },
+    { name: "Logout", to: "/logout" },
+  ]
+};
+
 const Main: FunctionComponent = () => {
+    const { company, links } = navigation;
     return (
       <div className="Main">
-        <Navbar />
+        <Navbar company={company} links={links} />
         <UserSummary />
         <RecommendationList />
       </div>
