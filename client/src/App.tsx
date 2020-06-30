011dcfb1c1a95ffc23e7c8758c0b1e1c077e8727
Pat Rivet <61415460+patrivet@users.noreply.github.com>
2020-06-30 18:02:27 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {} from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Main from './components/Main';
@@ -7,19 +7,14 @@ import Logout from './components/Logout';
 import UserProfile from './components/UserProfile';
 import GameDetail from './components/GameDetail';
 import About from './components/About';
-
-
-/*
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import Login from './components/Login/Login'
-import serverService from './services/ServerService'
-import hash from "./hash";
-import './App.css';
 import Navbar from './components/Navbar';
 import RecommendationList from './components/RecommendationList';
 import UserSummary from './components/UserSummary';
-*/
+
+import serverService from './services/ServerService'
+import hash from "./hash";
+
+import './App.css';
 
 
 function App() {
