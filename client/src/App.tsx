dbaded9bb4125df8788cf4ce84e6544ef1927e20
Lena Deo <o.deorditsa@gmail.com>
2020-06-30 15:34:13 +0100

    Client: Navbar component created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
-import React, {} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'reset-css';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      steamWreak app here!
-    </div>
-  );
+
+const navigation = {
+  company: { name: "SteamWreck", to: "/" },
+  links: [
+    { name: "About", to: "/about" },
+    { name: "FaveWreck", to: "/favourite" },
+    { name: "WorstWreck", to: "/worst" },
+    { name: "Logout", to: "/logout" },
+  ]
+};
+
+
+export default class App extends Component {
+  // the 'public' is a typescript feature.
+  public render() {
+    const { company, links } = navigation;
+
+    return (
+      <div className="App">
+        <Navbar company={company} links={links} />
+      </div>
+    );
+  }
 }
-
-export default App;
