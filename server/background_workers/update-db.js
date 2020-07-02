fc6c6da5fbba57cf11ee20bb83fa34b0d257a561
Juno <Laevino@gmail.com>
2020-07-02 13:33:37 -0400

    chore: Encapsulated update-db with a try catch for unresolved promises that occur if an error pops up.

--- server/background_workers/update-db.js
+++ server/background_workers/update-db.js
@@ -12,39 +12,43 @@ const { saveGame } = require('./../helpers/steam-api-helpers');
 // updateGames();
 
 const updateGames = async () => {
-  const users = await UserModel.find({});
-  const userGames = [];
+  try {
+    const users = await UserModel.find({});
+    const userGames = [];
 
-  // Extract all games from users and store them in userGames as a set (without repeated games)
-  for (let i = 0; i < users.length; i++) {
-    const user = users[i];
-    for (let j = 0; j < user.owned.games_owned.length; j++) {
-      const userGame = user.owned.games_owned[j];
-      if (!userGames.some((game) => game === userGame)) {
-        userGames.push(userGame);
+    // Extract all games from users and store them in userGames as a set (without repeated games)
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      for (let j = 0; j < user.owned.games_owned.length; j++) {
+        const userGame = user.owned.games_owned[j];
+        if (!userGames.some((game) => game === userGame)) {
+          userGames.push(userGame);
+        }
       }
     }
+
+    // Find all games in db and return an array of appids (game id from Steam).
+    const dbGames = await GameModel.find({});
+    const gameIds = dbGames.map((dbGame) => {
+      return dbGame.appid;
+    });
+
+    // From user games, find the games that are not in the db.
+    const gamesMissing = userGames.filter((userGame) => {
+      return !gameIds.includes(userGame.appid);
+    });
+
+    // Save all games that are not in the database already.
+    for (let i = 0; i < gamesMissing.length; i++) {
+      const game = gamesMissing[i];
+      await saveGame(game.appid, game.name);
+    }
+
+    console.log('DB Updater: All games updated!');
+    process.exit(-1);
+  } catch (error) {
+    console.error('Error: Problems saving games to database.');
   }
-
-  // Find all games in db and return an array of appids (game id from Steam).
-  const dbGames = await GameModel.find({});
-  const gameIds = dbGames.map((dbGame) => {
-    return dbGame.appid;
-  });
-
-  // From user games, find the games that are not in the db.
-  const gamesMissing = userGames.filter((userGame) => {
-    return !gameIds.includes(userGame.appid);
-  });
-
-  // Save all games that are not in the database already.
-  for (let i = 0; i < gamesMissing.length; i++) {
-    const game = gamesMissing[i];
-    await saveGame(game.appid, game.name);
-  }
-
-  console.log('DB Updater: All games updated!');
-  process.exit(-1);
 }
 
 module.exports = {
