11f8b57070915469ae6f1030e2a21bd6b26391d6
Pat Rivet <patrickmrivet@gmail.com>
2020-07-03 16:37:53 +0100

    Game card medium -show only 1 game in the component.

--- client/src/components/GameCardMedium/GameCardMedium.tsx
+++ client/src/components/GameCardMedium/GameCardMedium.tsx
@@ -29,32 +29,6 @@ const GameCardMedium: FunctionComponent<Props> = (props) => {
         </div>
       </div>
 
-      <h1 className="nameMedium">{props.recGame.name}</h1>
-      <div className ="allDetailsMedium">
-        <div className="mainDetailsMedium">
-          <img className="gameImg" src={`${props.recGame.background_image}`}></img>
-        </div>
-
-        <div className ="subDetailsMedium">
-          <div className="rating">Rating: <span className="ratingNumber">{props.recGame.rating * 100}</span></div>
-          <div className="reason">Wreck Reasoning: <span className="reasonTxt">{props.recGame.rating_reason}</span></div>
-          <div className="description">{htmlToText.fromString(props.recGame.description)}</div>          <div className="tags">{props.recGame.tags}</div>
-        </div>
-      </div>
-
-      <h1 className="nameMedium">{props.recGame.name}</h1>
-      <div className ="allDetailsMedium">
-        <div className="mainDetailsMedium">
-          <img className="gameImg" src={`${props.recGame.background_image}`}></img>
-        </div>
-
-        <div className ="subDetailsMedium">
-          <div className="rating">Rating: <span className="ratingNumber">{props.recGame.rating * 100}</span></div>
-          <div className="reason">Wreck Reasoning: <span className="reasonTxt">{props.recGame.rating_reason}</span></div>
-          <div className="description">{htmlToText.fromString(props.recGame.description)}</div>          <div className="tags">{props.recGame.tags}</div>
-          <div className="tags">{props.recGame.tags}</div>
-        </div>
-      </div>
     </div>
   )
 };
