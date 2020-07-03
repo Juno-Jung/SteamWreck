b1f5cfc0ce151d928e8495390f298c0459cb57e8
Robustoe <aligazitepe@gmail.com>
2020-07-03 11:50:20 +0200

    collapsible feature

--- client/src/components/GameCardMedium/GameCardMedium.tsx
+++ client/src/components/GameCardMedium/GameCardMedium.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent  } from 'react';
 import './GameCardMedium.scss'
 import Game from './../../Game'
+import Collapsible from 'react-collapsible';
 
 type GameCardMediumProps = {
   //
@@ -14,9 +15,11 @@ interface Props {
 const GameCardMedium: FunctionComponent<Props> = (props) => {
   return (
     <div>
-      <h1 className="nameMedium">{props.recGame.name}</h1>
-      <div className ="allDetailsMedium">
+      <Collapsible trigger=
+     { <h1 className="nameMedium">{props.recGame.name}</h1>}>
+
         <div className="mainDetailsMedium">
+      <div className ="allDetailsMedium">
           <img className="gameImg" src={`${props.recGame.background_image}`}></img>
         </div>
 
@@ -28,36 +31,9 @@ const GameCardMedium: FunctionComponent<Props> = (props) => {
           <div className="tags">{props.recGame.tags}</div>
         </div>
       </div>
+      </Collapsible>
 
-      <h1 className="nameMedium">{props.recGame.name}</h1>
-      <div className ="allDetailsMedium">
-        <div className="mainDetailsMedium">
-          <img className="gameImg" src={`${props.recGame.background_image}`}></img>
-        </div>
 
-        <div className ="subDetailsMedium">
-          <div className="rating">Rating: <span className="ratingNumber">{props.recGame.rating * 100}</span></div>
-          <div className="reason">Wreck Reasoning: <span className="reasonTxt">{props.recGame.rating_reason}</span></div>
-          <div className="purchased">X Days since purchase</div>
-          <div className="description">{props.recGame.description}</div>
-          <div className="tags">{props.recGame.tags}</div>
-        </div>
-      </div>
-
-      <h1 className="nameMedium">{props.recGame.name}</h1>
-      <div className ="allDetailsMedium">
-        <div className="mainDetailsMedium">
-          <img className="gameImg" src={`${props.recGame.background_image}`}></img>
-        </div>
-
-        <div className ="subDetailsMedium">
-          <div className="rating">Rating: <span className="ratingNumber">{props.recGame.rating * 100}</span></div>
-          <div className="reason">Wreck Reasoning: <span className="reasonTxt">{props.recGame.rating_reason}</span></div>
-          <div className="purchased">X Days since purchase</div>
-          <div className="description">{props.recGame.description}</div>
-          <div className="tags">{props.recGame.tags}</div>
-        </div>
-      </div>
     </div>
   )
 };
