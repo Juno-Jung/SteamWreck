92089d9d3a6074549ab52e6eabdcc86959e9c446
Pat Rivet <patrickmrivet@gmail.com>
2020-07-03 10:12:39 +0100

    fix: corrected game card large style - altered by merge

--- client/src/components/GameCardLarge/GameCardLarge.tsx
+++ client/src/components/GameCardLarge/GameCardLarge.tsx
@@ -3,52 +3,31 @@ import './GameCardLarge.scss'
 import Game from './../../Game'
 const htmlToText = require("html-to-text");
 
-type GameCardLargeProps = {
-  //
-};
-
-
 interface Props {
   // Top Recommended Game.
-  recGame: Game;
+  recGame: Game
 }
 
 const GameCardLarge: FunctionComponent<Props> = (props) => {
   return (
     <div>
-      {props.recGame && (
-        <div>
-          <h1 className="nameLarge">{props.recGame.name}</h1>
-          <div className="allDetailsLarge">
-            <div className="mainDetailsLarge">
-              <img
-                className="gameImg"
-                src={`${props.recGame.background_image}`}
-              ></img>
-              {/*           <h4 className="purchased">X Days since purchase</h4> */}
-            </div>
-          </div>
-
-          <div className="subDetailsLarge">
-            <div className="rating">
-              Rating:{" "}
-              <span className="ratingNumber">{`${(
-                100 * props.recGame.rating
-              ).toFixed(0)} / 100`}</span>
-            </div>
-            <div className="reason">
-              Wreck Reasoning:{" "}
-              <span className="reasonTxt">{props.recGame.rating_reason}</span>
-            </div>
-            <div className="description">
-              {htmlToText.fromString(props.recGame.description)}
-            </div>
-            <div className="tags">{props.recGame.tags}</div>
-          </div>
+      <h1 className="nameLarge">{props.recGame.name}</h1>
+      <div className ="allDetailsLarge">
+        <div className="mainDetailsLarge">
+          <img className="gameImg" src={`${props.recGame.background_image}`}></img>
         </div>
-      )}
+
+        <div className ="subDetailsLarge">
+          <div className="rating">Rating: <span className="ratingNumber">{`${(
+                100 * props.recGame.rating
+              ).toFixed(0)} / 100`}</span></div>
+          <div className="reason">Wreck Reasoning: <span className="reasonTxt">{props.recGame.rating_reason}</span></div>
+          <div className="description">{htmlToText.fromString(props.recGame.description)}</div>
+          <div className="tags">{props.recGame.tags}</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 };
 
 export default GameCardLarge;
