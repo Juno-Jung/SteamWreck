f419c9b9d4b32b915dbe2425b1f27c1c343686bb
Pat Rivet <patrickmrivet@gmail.com>
2020-07-06 18:57:14 +0100

    client-game-detail: add drilldown from Game card large to Game detail component.

--- client/src/components/GameDetail/GameDetail.tsx
+++ client/src/components/GameDetail/GameDetail.tsx
@@ -1,11 +1,29 @@
 import React, { FunctionComponent  } from 'react';
+import Game from '../../Game';
+import './GameDetail.scss'
 
-type GameDetailProps = {
-  //
+
+interface Props {
+  location: any // not sure what type this is??
 };
 
-const GameDetail: FunctionComponent = () => {
-  return <div>GameDetail</div>;
+const GameDetail: FunctionComponent<any> = (props) => {
+  const game: Game = props.location.state;
+  return (
+    <div className="gameDetail">
+
+      <h1 className="gameDetail__name">{game.name}</h1>
+      <div className="gameDetail__screenshots">
+          screenshots component to go here
+      </div>
+
+
+      <div className="gameDetail__longDescription">{game.description_steam}</div>
+
+
+      <div className="gameDetail__reviewLink">{game.appid}</div>
+    </div>
+  )
 };
 
 export default GameDetail;
