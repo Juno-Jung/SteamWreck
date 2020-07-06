949da1e5b508d143314706455ff1de8bbea639d7
Pat Rivet <patrickmrivet@gmail.com>
2020-07-06 22:23:06 +0100

    chore: removed console logs and unused props.

--- client/src/components/GameDetail/GameDetail.tsx
+++ client/src/components/GameDetail/GameDetail.tsx
@@ -1,9 +1,8 @@
 import React, { FunctionComponent  } from 'react';
-import htmlToFormattedText from "html-to-formatted-text";
 import Gallereact from 'gallereact';
 import Game from '../../Game';
 import './GameDetail.scss'
-const BASE_URL = 'https://store.steampowered.com/app';
+const STEAM_STORE_BASE_URL = 'https://store.steampowered.com/app';
 
 interface Props {
   location: any // not sure what type this is??
@@ -12,43 +11,22 @@ interface Props {
 const GameDetail: FunctionComponent<any> = (props) => {
   const game: Game = props.location.state;
 
-  console.log('URL=',game.metacritic_url);
-  console.log('screenshots=');
-
-
-
-
-
-  const screenshots = game.screenshots.map( e => {
-    return {...e, image : e.path_full}
+  const screenshots = game.screenshots.map( screenshot => {
+    // add an image prop to each screenshot object.
+    return {...screenshot, image : screenshot.path_full}
   });
 
+  const steamGameLink = `${STEAM_STORE_BASE_URL}/${game.appid}`;
+  const htmlDesc = { __html: game.description_steam};
 
-  const images: any = [];
-  game.screenshots.forEach( (e) => {
-    images.push({image : e.path_full})
-  });
-
-  console.log(images);
-
-
-  const steamGameLink = `${BASE_URL}/${game.appid}`
-
-  const htmlDesc = { __html: game.description_steam}
   return (
     <div className="gameDetail">
-
       <h1 className="gameDetail__name">{game.name}</h1>
       <div className="gameDetail__screenshots">
-        <Gallereact images={images} displayPreview={true} />
+        <Gallereact images={screenshots} displayPreview={true} />
       </div>
 
-
-
-
-
       <div className="gameDetail__longDescription" dangerouslySetInnerHTML={htmlDesc}></div>
-
       <a className="gameDetail__metricCriticReviewLink" href={game.metacritic_url}>{game.name}</a>
       <a className="gameDetail__steamGameLink" href={steamGameLink}>TBD</a>
     </div>
