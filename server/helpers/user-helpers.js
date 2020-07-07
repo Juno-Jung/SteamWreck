853ce6bf1a8e1e27429335a569f33355e4a2b481
Juno <Laevino@gmail.com>
2020-07-07 12:58:32 -0400

    refactor: getGameRecommendations to now return an array with each type of recommendation instead of returning each one a call.

--- server/helpers/user-helpers.js
+++ server/helpers/user-helpers.js
@@ -5,57 +5,52 @@ const UserModel = require('../models/user');
 
 const { getTagsAndGenres, rateGames } = require('./recommendations-helpers');
 
-const getGameRecommendations = async (user, type, max = 3, friends, friendsLibrary, ratingType = 'similarity') => {
+const getGameRecommendations = async (user, max = 3, friends, friendsLibrary, ratingType = 'similarity') => {
   try {
-    let userGames;
+    let userGamesTotal;
+    let userGamesRecent;
+    let userGamesWorst;
     let accuracy;
 
-    if (type === 'total') {
-      // For logging purposes        
-      console.log('\n', 'Recommendation Type: Total');
-      // Sort games by total playtime from increasing to decreasing
-      userGames = user.owned.games_owned.slice().sort((a, b) => {
-        return b.playtime_forever - a.playtime_forever;
+    // Sort games by total playtime from increasing to decreasing
+    userGamesTotal = user.owned.games_owned.slice().sort((a, b) => {
+      return b.playtime_forever - a.playtime_forever;
+    });
+
+    // Sort games by recent playtime from increasing to decreasing
+    userGamesRecent = user.owned.games_owned.filter((game) => game.playtime_2weeks)
+      .sort((a, b) => {
+        return b.playtime_2weeks - a.playtime_2weeks;
       });
 
-      // Sets the base number of games we compare against
-      accuracy = 3;
-    } else if (type === 'recent') {
-      // For logging purposes
-      console.log('\n', 'Recommendation Type: Recent');
-      // Sort games by recent playtime from increasing to decreasing
-      userGames = user.owned.games_owned.filter((game) => game.playtime_2weeks)
-        .sort((a, b) => {
-          return b.playtime_2weeks - a.playtime_2weeks;
-        });
+    // Sort games by recent playtime from increasing to decreasing
+    userGamesWorst = user.owned.games_owned.filter((game) => {
+      return ((10 < game.playtime_forever) && (game.playtime_forever < 60));
+    })
+      .sort((a, b) => {
+        return a.playtime_forever - b.playtime_forever;
+      });
 
-      // Sets the base number of games we compare against
-      accuracy = 3;
-    } else if (type === 'worst') {
-      // For logging purposes
-      console.log('\n', 'Recommendation Type: Worst');
-      // Sort games by recent playtime from increasing to decreasing
-      userGames = user.owned.games_owned.filter((game) => {
-        return ((10 < game.playtime_forever) && (game.playtime_forever < 60));
-      })
-        .sort((a, b) => {
-          return a.playtime_forever - b.playtime_forever;
-        });
-
-      // Sets the base number of games we compare against
-      accuracy = 3;
-    }
-
-    console.log(`Top ${accuracy} Games for ${type.toUpperCase()}: `, userGames.slice(0, accuracy));
+    accuracy = 3;
 
     // Gets all tags and genres of top three games as objects. topTagsAndGenres returns an array with two entries, first is an object of tag/playtime pairs, second is an object of genre/playtime pairs.
-    const [tags, genres] = await getTagsAndGenres(userGames.slice(0, accuracy), user.owned.game_ids, type);
+    const [totalTags, totalGenres] = await getTagsAndGenres(userGamesTotal.slice(0, accuracy), user.owned.game_ids, 'total');
+    const [recentTags, recentGenres] = await getTagsAndGenres(userGamesRecent.slice(0, accuracy), user.owned.game_ids, 'recent');
+    const [worstTags, worstGenres] = await getTagsAndGenres(userGamesWorst.slice(0, accuracy), user.owned.game_ids, 'worst');
 
     // Rates unplayed games by recommendation algorithm. Returns array of unplayed games in the order of the highest rating to lowest rating. (Rating is not added to objects);
-    const ratedUnplayed = await rateGames(user.owned.games_unplayed, tags, genres, user.owned.game_unplayed_ids, friends, friendsLibrary, ratingType);
+    console.log('\n', 'Recommendation Type: Total');
+    console.log(`Top ${accuracy} Games for Total: `, userGamesTotal.slice(0, max));
+    const totalRated = await rateGames(user.owned.games_unplayed, totalTags, totalGenres, user.owned.game_unplayed_ids, friends, friendsLibrary, ratingType);
+    console.log('\n', 'Recommendation Type: Recent');
+    console.log(`Top ${accuracy} Games for Recent: `, userGamesRecent.slice(0, max));
+    const recentRated = await rateGames(user.owned.games_unplayed, recentTags, recentGenres, user.owned.game_unplayed_ids, friends, friendsLibrary, ratingType);
+    console.log('\n', 'Recommendation Type: Worst');
+    console.log(`Top ${accuracy} Games for Worst: `, userGamesWorst.slice(0, max));
+    const worstRated = await rateGames(user.owned.games_unplayed, worstTags, worstGenres, user.owned.game_unplayed_ids, friends, friendsLibrary, ratingType);
 
-    // Returns top three recommendations
-    return ratedUnplayed.slice(0, max);
+    // Returns top three recommendations for each type.
+    return [totalRated.slice(0, max), recentRated.slice(0, max), worstRated.slice(0, max)]
   } catch (error) {
     console.error(error);
   }
