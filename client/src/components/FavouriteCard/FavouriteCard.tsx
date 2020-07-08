791b271fbb1841ded5f98fe4a0ee7fa10d926561
Pat Rivet <patrickmrivet@gmail.com>
2020-07-08 21:37:19 +0100

    Favourites -final style changes for now.

--- client/src/components/FavouriteCard/FavouriteCard.tsx
+++ client/src/components/FavouriteCard/FavouriteCard.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent  } from 'react';
 import Game from '../../Game';
 import GameRating from '../GameRating';
+import RecommendationReason from '../RecommendationReason';
 import { Link } from 'react-router-dom';
 import './FavouriteCard.scss';
 
@@ -23,7 +24,10 @@ const FavouriteCard: FunctionComponent<Props> = (props) => {
         <Link to={linkPathAndContent}><img className="favouriteGame__image box--hover" src={props.game.background_image} alt=""/></Link>
         <div className="favouriteGame__ratingAndDate">
           <GameRating game={props.game}/>
-          <div>Bookmarked: TBC</div>
+          <div className="favouriteGame__ratingAndDate__date">Bookmarked: TBC</div>
+        </div>
+        <div className="favouriteGame__recReason">
+          <RecommendationReason reasoning={props.game.rating_reason}/>
         </div>
       </div>
 
