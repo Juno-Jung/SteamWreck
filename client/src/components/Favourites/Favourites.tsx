508aab78161f0214d7f84faffc6b2337d27353b9
Pat Rivet <patrickmrivet@gmail.com>
2020-07-08 17:10:44 +0100

    Favourites harvested from server and showing in favourites component.

--- client/src/components/Favourites/Favourites.tsx
+++ client/src/components/Favourites/Favourites.tsx
@@ -1,44 +1,23 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 import Game from '../../Game';
 import FavouriteCard from '../FavouriteCard';
-import serverService from "../../services/ServerService";
+import {FavGamesContext} from '../../App';
 
 interface Props {
-  //favs: [Game];
-  favs: Array<Game>;
+  favGames: Array<Game>;
+  history: any;
 };
 
-/* TEMP FETCH the FAVs HERE - using hard coded steam-id */
-let favs: [string];
-let favGames: Array<Game> = [];
-console.info('INFO: hi from Favourites.tsx.........');
-
 const Favourites: FunctionComponent<Props> = (props) => {
-  //Fetch User and Recommendations using a Promise All
-  useEffect( () => {
-    serverService.getUserInfo('76561198056384406')
-    .then( (data) => {
-      // Handle getUserInfo promise
-      if (data.user) {
-        favs = data.user.favourites;
-      }
-    })
-    .then( ()=> {
-      // fetch the fav games ...
-      serverService.getGames([123,456,789])
-      .then( (data) => {
-        favGames = data;
-      })
-    })
-  }, []);
+  const favGames = useContext(FavGamesContext);
 
   return (
     <>
       <div className="favouriteslist">
       <h1 className="favouriteslist__title">Liked Wreckomendations</h1>
       <div className="favouriteslist__favs">
-        {favGames.map( fav => {
-          return <FavouriteCard game={fav}/>
+        {favGames.map( game => {
+          return <FavouriteCard game={game}/>
         })}
       </div>
       </div>
